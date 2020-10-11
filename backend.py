2159f98847a495728a46cb0ba2a7d53c11372f02
KvaWenKa <apc156@yandex.ru>
2020-10-11 11:46:18 +0300

    Initial commit

---
+++ backend.py
@@ -0,0 +1,41 @@
+from tkinter import messagebox
+
+dict_special = {"eins": 1, "zenh": 10, "elf": 11, "zwolf": 12, "sechzehn": 16, "siebzehn": 17, "dreibig": 30}
+dict_endings = {"zig": 10, "zehn": 10, "hundert": 100}
+dict_prefixes = {"ein": 1, "zwei": 2, "zwan": 2, "drei": 3, "vier": 4, "funf": 5, "sechs": 6, "sieben": 7, "acht": 8, "neun": 9}
+dict_oldrus_numbers = {500: "Ф", 100: "Р", 30: "Л", 8: "И", 2: "В", 1: "А"}
+
+def search_number(str_number):
+    for spec in dict_special:
+        if str_number == spec:
+            return dict_special[spec]
+    else:
+        for prefix in dict_prefixes:
+            if str_number.startswith(prefix):
+                for ending in dict_endings:
+                    if str_number.endswith(ending):
+                        if prefix + ending != str_number:
+                            messagebox.showwarning("Внимание",
+                                                   f"Ошибка в написании числа {prefix + ending} ({str_number}) ")
+                            return 0
+                        elif ending == "zehn":
+                            return 10 + dict_prefixes[prefix]
+                        else:
+                            return dict_prefixes[prefix] * dict_endings[ending]
+                else:
+                    if prefix != str_number:
+                        messagebox.showwarning("Внимание",
+                                               f"Ошибка в написании числа {prefix} ({str_number}) ")
+                    else:
+                        return dict_prefixes[prefix]
+        else:
+            messagebox.showerror("Number not found", f"{str_number} - не известное число")
+            return 0
+
+def to_oldRus_numbers(numbers):
+    oldRus = ""
+    for old_num in dict_oldrus_numbers:
+        while old_num <= numbers:
+            numbers -= old_num
+            oldRus += dict_oldrus_numbers[old_num]
+    return oldRus
---
+++ main.py
@@ -0,0 +1,41 @@
+from tkinter import *
+from tkinter import messagebox
+import backend
+
+def button_click():
+    general_num = 0
+    spl_str = (str_num.get()).split()
+    i = 0
+    while i < len(spl_str):
+        if spl_str[i] == "und":
+            i += 1
+            general_num += backend.search_number(spl_str[i])
+        elif len(str(general_num)) > len(str(backend.search_number(spl_str[i]))) or general_num == 0:
+            general_num += backend.search_number(spl_str[i])
+        else:
+            messagebox.showerror("Error", "Не коректный ввод")
+            return
+        i += 1
+    label1.config(text=str(general_num))
+    label2.config(text=backend.to_oldRus_numbers(general_num))
+
+root = Tk()
+root.title("Перовод чисел с Немецкого в СтарРус")
+root.geometry("400x300")
+root.config(bg="#dce0ce")
+
+str_num = StringVar()
+
+btn = Button(text="Перевод", background ="#dcdcdc", width="7", font="5", command=button_click)
+btn.place(relx=.5, rely=.3, anchor="c")
+
+textbox1 = Entry(textvariable=str_num, width="30", font = "5")
+textbox1.place(relx=.5, rely=.1, anchor="c")
+
+label1 = Label(text ="Араб", bg="#dce0ce", width = "10", font = "5")
+label1.place(relx=.2, rely=.5, anchor="c")
+
+label2 = Label(text ="СтарРус", bg="#dce0ce", width = "10", font = "5")
+label2.place(relx=.8, rely=.5, anchor="c")
+
+root.mainloop()
